18ae2d13a96d55b0680851dffcd9dd96992e88d6
Vipul Choudhary <vipulchoudhary015@gmail.com>
2024-06-05 11:47:45 +0530

    modified:   src/index.css 	modified:   src/pages/Login/Login.jsx 	modified:   tailwind.config.js

--- front/notes-app/src/pages/Login/Login.jsx
+++ front/notes-app/src/pages/Login/Login.jsx
@@ -6,17 +6,17 @@ const Login = () => {
   return (
     <>
       <Navbar/>
-      <div className='flex items-center justify-center mt-28'>
-        <div className=''>
+      <div className='  flex items-center justify-center mt-28'>
+        <div className='w-96 border more-rounded bg-white px-7 py-10'>
           <form onSubmit={()=>{}}>
-            <h4>Login</h4>
-            <input type='text' placeholder='Enter Username' className='input-box'/>
-            <input type='password' placeholder='Enter Password' className='input-box'/>
+            <h4 className='text-2xl mb-7'>Login</h4>
+            <input type='text' placeholder='Enter Email' className='input-box'/>
+            
 
             <button type='submit' className='btn-primary'>Login</button>
             <p className='text-sm text-center mt-4'>
               Not Registered? {""}
-              <Link to='/signUp'>
+              <Link to='/signUp' className='font-medium text-primary underline'>
                 Create an Account
               </Link>
             </p>
