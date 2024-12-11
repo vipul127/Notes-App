c91fbb31c6a3ca3d3bbf90c89b2ebdd379ea4d88
Vipul Choudhary <125580392+vipul127@users.noreply.github.com>
2024-12-11 18:18:43 +0530

    routes

--- front/notes-app/src/App.jsx
+++ front/notes-app/src/App.jsx
@@ -1,30 +1,28 @@
-import React from 'react'
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import Home from './pages/Home/Home'
-import Login from './pages/Login/Login'
-import SignUp from './pages/SignUp/SignUp'
-import Welcome from './pages/SignUp/welcome'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import Login from './pages/Login/Login';
+import SignUp from './pages/SignUp/SignUp';
+import Welcome from './pages/SignUp/Welcome';
 
-
-const routes=(
+const routes = (
   <Router>
     <Routes>
-      <Route path="/dashboard" exact element= {<Home/>} />
-      <Route path="/login" exact element= {<Login/>} />
-      <Route path="/SignUp" exact element= {<SignUp/>} />
-      <Route path="/welcome" exact element= {<Welcome/>} />
+      <Route path="/" element={<Navigate to="/dashboard" />} />
+      <Route path="/dashboard" exact element={<Home />} />
+      <Route path="/login" exact element={<Login />} />
+      <Route path="/SignUp" exact element={<SignUp />} />
+      <Route path="/welcome" exact element={<Welcome />} />
     </Routes>
   </Router>
-)
+);
 
 const App = () => {
   return (
-    <>
     <div>
-    {routes}
+      {routes}
     </div>
-    </>
-    )
+  );
 };
 
-export default App
+export default App;
